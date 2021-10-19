573a64af4cfc2dd407f833d700c0ca00b1bc6820
pandoragear <andy.dang.it@gmail.com>
2021-10-18 22:06:01 -0500

    Remove not needed spac

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,21 @@
-import Navbar from '../Components/NavBar/Navbar';
-import Home from '../Pages/Home/Home'
+import Navbar from "../Components/NavBar/Navbar";
+import Home from "../Pages/Home/Home";
+import Quiz from "../Pages/Quiz/Quiz";
+import About from "../Pages/About/About";
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Navbar /> 
-      <Switch> 
-        <Route path='/' exact component={Home} /> 
-        </Switch> 
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/quiz" component={Quiz} />
+        <Route path="/about" component={About} />
+      </Switch>
     </Router>
   );
 }
--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import {Nav, NavLink, Bars, NavMenu } from './NavBarElements'
+import React from "react";
+import { Nav, NavLink, Bars, NavMenu } from "./NavBarElements";
 
 const Navbar = () => {
-    return (
-        <div>
-            <Nav> 
-                <NavLink to="/">
-                    <h1> Logo</h1> 
-                </NavLink>
-                <Bars /> 
-                <NavMenu>
-                    <NavLink to="/" activeStyle> 
-                    Home
-                    </NavLink>
-                    <NavLink to="/quiz" activeStyle> 
-                    Quiz
-                    </NavLink>
-                    <NavLink to="/about" activeStyle> 
-                    About
-                    </NavLink>
-                </NavMenu>
-            </Nav>
-        </div>
-    );
+  return (
+    <div>
+      <Nav>
+        <NavLink to="/">
+          <h1> Logo</h1>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/" activeStyle>
+            Home
+          </NavLink>
+          <NavLink to="/quiz" activeStyle>
+            Quiz
+          </NavLink>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+        </NavMenu>
+      </Nav>
+    </div>
+  );
 };
 
 export default Navbar;
