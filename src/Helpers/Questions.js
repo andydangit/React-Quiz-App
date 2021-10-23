3704d72161b446f37ee18fab641dd8cbaa1efbe8
pandoragear <andy.dang.it@gmail.com>
2021-10-22 21:31:09 -0500

    add questionAire with multiple Choice

---
+++ src/Helpers/Questions.js
@@ -0,0 +1,19 @@
+export const Questions = [
+    {
+        prompt: "What is React.js", 
+        optionA: "Library",
+        optionB: "FrameWork",
+        optionC: "Object", 
+        optionD: "The Matrix",
+        answer: "optionA",
+    },
+
+    {
+        prompt: "What is a Props", 
+        optionA: "Virtual DOM",
+        optionB: "HTML Attributes",
+        optionC: "Arguments passed into React Components", 
+        optionD: "Track the state in a function",
+        answer: "optionC",
+    }
+]
