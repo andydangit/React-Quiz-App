432a2ab2d42ed8a4652fa062763cba31fb07da12
pandoragear <andy.dang.it@gmail.com>
2021-10-23 18:29:49 -0500

    add semantic css from cdn

--- src/Pages/ContactUs/Contact.jsx
+++ src/Pages/ContactUs/Contact.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+
+import ContactUsId from "../ContactUs/ContactUsId";
 
 const ContactUs = () => {
-    return (
-        <div>
-            <h1> Contact US  </h1> 
-        </div>
-    );
+  const styleLink = document.createElement("link");
+  styleLink.rel = "stylesheet";
+  styleLink.href =
+    "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+  document.head.appendChild(styleLink);
+
+  return (
+    <div>
+      <ContactUsId />
+    </div>
+  );
 };
 
 export default ContactUs;
