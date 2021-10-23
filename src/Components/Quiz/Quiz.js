d6ce6280762531513c582e2b3c71a9562af4d0c1
pandoragear <andy.dang.it@gmail.com>
2021-10-22 23:23:55 -0500

    import question as button

--- src/Components/Quiz/Quiz.js
+++ src/Components/Quiz/Quiz.js
@@ -9,6 +9,12 @@ function Quiz() {
     return (
         <div className="Quiz">
             <h1>{Questions[currentQuestion].prompt} </h1>
+            <div className="options">
+                <button>{Questions[currentQuestion].optionA} </button>
+                <button>{Questions[currentQuestion].optionB} </button>
+                <button>{Questions[currentQuestion].optionC} </button>
+                <button>{Questions[currentQuestion].optionD} </button>
+            </div>
         </div>
     )
 }
