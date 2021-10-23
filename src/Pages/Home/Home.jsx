5166094e352a657c16b06517f67833b9ef6d1f0e
pandoragear <andy.dang.it@gmail.com>
2021-10-22 20:51:52 -0500

    import useContext

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,10 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
+
 import GameOver from '../../Components/GameOver/GameOver';
 import MainMenu from '../../Components/MainMenu/MainMenu';
 import Quiz from '../../Components/Quiz/Quiz';
+
+import { QuizContext } from "../../Helpers/Contexts";
 
 function Home() {
 const [gameState, setGameState] = useState("menu");
@@ -9,7 +12,7 @@ const [gameState, setGameState] = useState("menu");
     return (
         <div>
             <h1> Home</h1> 
-            
+
             {gameState === "menu" && <MainMenu />}
             {gameState === "Quiz" && <Quiz />}
             {gameState === "GameOver" && <GameOver />}
