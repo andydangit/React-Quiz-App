614df362a4c461fe0599af1716d0f0966e5bf6fc
pandoragear <andy.dang.it@gmail.com>
2021-10-27 21:52:52 -0500

    update About info page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1> About </h1> 
+            <h3> This react quiz was to create as a short study guide to React vocabulary   </h3> 
         </div>
     );
 };
