7e0637a098af86ac82760cd37c518ee3716cdcbb
pandoragear <andy.dang.it@gmail.com>
2021-10-28 22:18:41 -0500

    load data from json file

--- src/posts/posts.js
+++ src/posts/posts.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import SampleAnswer from '../Data/SampleAnswer.json'
 
 function Posts() {
     return (
         <div>
-            <h2> answer </h2> 
+            {SampleAnswer.map((detail) => {
+                return <div> 
+                    <h2> {detail.Question} </h2>
+                <p>{detail.Answer} </p> 
+                </div> 
+            })}
         </div>
     )
 }
