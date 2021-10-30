bd1a52e79038aa26a3cf7c559973045708bfe706
pandoragear <andy.dang.it@gmail.com>
2021-10-29 20:23:49 -0500

    testing to modify with axios

---
+++ src/Components/List.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import Card from "./Card";
+import db from '../../src/'
+
+function UserList() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const getUser = async () => {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+      setUsers(res.data);
+    };
+    getUser();
+  }, []);
+
+  return (
+    <div>
+      {users.map((user) => {
+        return (
+          <div>
+            <Card user={user} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default UserList;
