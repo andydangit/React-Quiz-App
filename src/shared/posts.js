0f8373a4cbdab33015ce14946642118996e0b51d
pandoragear <andy.dang.it@gmail.com>
2021-11-04 23:51:03 -0500

    test

---
+++ src/shared/posts.js
@@ -0,0 +1,12 @@
+export const EPISODES = [
+    {
+        id: 1,
+        name: "Chapter 1",
+        sum_short: "A Mandalorian bounty hunter tracks a target for a well-paying client.",
+        sum_long: "Five years after the fall of the Galactic Empire a Mandalorian bounty hunter hands his latest bounty to Greef Karga. Then he accepts an under-the-table commission on the outpost world of Nevarro from an enigmatic client with apparent Imperial connections, directing him to track down and capture an unnamed fifty-year-old target. While the Client is indifferent to the target's well-being, his colleague Dr. Pershing insists the target be brought back alive. The Mandalorian is given a down payment of a single bar of Beskar steel, sacred to his people. He takes it to a covert Mandalorian enclave where an armorer uses it to make him a pauldron. Arriving at the planet of the target's last reported location, the Mandalorian is aided by a vapor farmer named Kuiil. Tired of the chaos that bounty hunters bring to the area, Kuiil leads him to the target's location and departs. Entering the remote and heavily defended encampment, the Mandalorian reluctantly teams up with bounty hunting droid IG-11 to clear the camp and find the quarry– a child of Yoda's species. When IG-11 attempts to kill the infant per its bounty orders, the Mandalorian shoots and destroys the droid, taking the Child alive.",
+        director: "Dave Filoni",
+        writer: "Jon Favreau",
+        color: "eptitle eptitlecolor1",
+        colorcard: "epcardcolor1 stretched-link"
+    },
+];
