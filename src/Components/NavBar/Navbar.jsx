b0af17380da757ea2149b42397256af6368feba7
pandoragear <andy.dang.it@gmail.com>
2021-11-05 03:13:00 -0500

    add info to navbar

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -1,28 +1,58 @@
-import React from "react";
-import { Nav, NavLink, Bars, NavMenu } from "./styles";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+// import { Button } from "./Button";
+import './Navbar.css';
 
-const Navbar = () => {
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true)
+
+  const handleClick = () => setClick(!click);
+  const closeMenu = () => setClick(false);
+
+  const showButton = () => {
+      if(window.innerWidth <= 960) {
+          setButton(false);
+      } else{
+          setButton(true);
+      }
+  };
+
+  window.addEventListener('resize', showButton);
+
   return (
-    <div>
-      <Nav>
-        <NavLink to="/">
-          <h1> Logo</h1>
-        </NavLink>
-        <Bars />
-        <NavMenu>
-          <NavLink to="/" activeStyle>
-            Home
-          </NavLink>
-          <NavLink to="/about" activeStyle>
-            About
-          </NavLink>
-          <NavLink to="/contact" activeStyle>
-            Contact Us
-          </NavLink>
-        </NavMenu>
-      </Nav>
-    </div>
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick ={closeMenu}>
+            {/* A icon  */}
+           React Quiz
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/About" className="nav-links" onClick={closeMenu}>
+                About
+              </Link>
+            </li>
+            <li className="nav-item" >
+              <Link to="/Contact" className="nav-links" onClick={closeMenu}>
+                Contact
+              </Link>
+            </li>
+          </ul>
+          {/* {button && <Button buttonStyle="btn--outline"> Projects </Button>} */}
+        </div>
+      </nav>
+    </>
   );
-};
+}
 
 export default Navbar;
